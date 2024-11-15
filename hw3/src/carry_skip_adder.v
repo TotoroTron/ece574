c809fe8a0923d2bbb701fdce316a6831b8b15aa6
TotoroTron <brian.cheng.1997@gmail.com>
2024-11-15 17:19:02 -0500

    hw3 submit

---
+++ hw3/src/carry_skip_adder.v
@@ -0,0 +1,16 @@
+module carry_skip_adder
+#(
+    parameter DATA_WIDTH = 16
+)(
+    input wire [DATA_WIDTH-1:0] iv_a,
+    input wire [DATA_WIDTH-1:0] iv_b,
+    input wire i_cin,
+    output wire [DATA_WIDTH-1:0] ov_sum,
+    output wire o_cout
+);
+
+    // WIP
+
+
+
+endmodule
---
+++ hw3/src/skip_logic.v
@@ -0,0 +1,20 @@
+module skip_logic
+#(
+    parameter BLOCK_WIDTH = 4
+)(
+    input wire [BLOCK_WIDTH-1:0] iv_a,
+    input wire [BLOCK_WIDTH-1:0] iv_b,
+    input wire i_cin,
+    input wire i_rca_cout,
+    output wire o_cout
+);
+
+    wire [BLOCK_WIDTH-1:0] p;
+    wire block_prop;
+    
+    // WIP!
+
+
+endmodule
+    
+
